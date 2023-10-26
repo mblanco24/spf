621701d06ff3b399cf2752825cd9412c0a72f87e
mblanco24 <112362860+mblanco24@users.noreply.github.com>
2023-10-26 00:08:26 -0400

    Delete apirest_crud_laravel8-main directory

--- apirest_crud_laravel8-main/routes/borrar.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-
-
-Route::GET('/articulos', 'ArticuloController@index');
-Route::POST('/articulos', 'ArticuloController@store');
-Route::PUT('/articulos/{id}', 'ArticuloController@update');
-Route::DELETE('/articulos/{id}', 'ArticuloController@destroy');
